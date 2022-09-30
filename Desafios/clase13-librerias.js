3986a8f2a82a69cb581069b2c310e0d494ec9274
Tu Nombre <piaggio_09@hotmail.com>
2022-09-29 21:53:23 -0300

    Desafio Clase N°13

--- Desafios/clase13-librerias.js
+++ Desafios/clase13-librerias.js
@@ -65,18 +65,18 @@ const agregarCarrito = () => {
       Toastify({
         text: "Añadido al Carrito",
         offset: {
-          x: 10, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
-          y: 50, // vertical axis - can be a number or a string indicating unity. eg: '2em'
+          x: 10,
+          y: 50,
         },
         style: {
           background: "linear-gradient(to right, #a0a1a1, #303030",
         },
-        duration: 3000,
+        duration: 1500,
       }).showToast();
       iconCarrito.innerHTML = `
       <i class="bi bi-cart3"></i>
       <i class="bi bi-1-circle-fill"></i>`;
-    };
+    }; // Falta pushear elementos al carrito y modal carrito
   }
 };
 
@@ -125,7 +125,7 @@ const buscar = () => {
     mostrarCatalogo();
   };
   mostrarCampoBuscar(catalogo);
-  carritoIcon(document.querySelector(".barraBuqueda"));
+  carritoIcon(document.querySelector(".titulo"));
 };
 
 /* func display icon carrito */
@@ -184,6 +184,7 @@ const mostrarCampoBuscar = (apendearA) => {
       });
       resetarCatalogo();
     }
+    agregarCarrito();
   }
 };
 
@@ -203,150 +204,7 @@ const resetarCatalogo = () => {
             </div>`;
     catalogoContainer.appendChild(catalogo);
   });
+  agregarCarrito();
 };
-
-/* CODIGO DE DESAFIO ANTERIOR. */
-
-function menu2() {
-  opcion = Number(
-    prompt(`Ingrese una opcion:
-    Ingrese el id para agregar al carrito.
-    1. Nueva busqueda.
-    2. Ver Catálogo. (ver resultados en consola).
-    3. Ver Carrito.
-    0. Salir`)
-  );
-  selector();
-}
-
-function menu3() {
-  opcion = Number(
-    prompt(`Ingrese una opcion:
-    1. Nueva busqueda.
-    2. Ver Catálogo. (ver resultados en consola).
-    4. Finalizar Compra.
-    0. Salir`)
-  );
-  selector();
-}
-
-function selector() {
-  if (opcion == 1) {
-    buscarZapatillas();
-  } else if (
-    opcion == 10 ||
-    opcion == 20 ||
-    opcion == 30 ||
-    opcion == 40 ||
-    opcion == 50 ||
-    opcion == 60
-  ) {
-    const resultadosBusqueda = zapatillas.find((el) => el.id == opcion);
-    carrito.push(resultadosBusqueda);
-    alert(
-      `${resultadosBusqueda.marca} ${resultadosBusqueda.modelo} se agregó al carrito correctamente.`
-    );
-    menu2();
-  } else if (opcion == 2) {
-    console.log(zapatillas);
-    menu2();
-  } else if (opcion == 3) {
-    console.log(carrito);
-    calcularCarrito();
-    console.log(`Total del Carrito: $${total}`);
-    menu3();
-  } else if (opcion == 4) {
-    finalizarCompra();
-    despedir();
-  } else if (opcion == 0) {
-    despedir();
-  } else {
-    alert("Ingrese una opción correcta");
-    menu1();
-  }
-}
-
-/* Buscar zapatillas */
-let entrada;
-function buscarZapatillas() {
-  entrada = prompt(
-    "Ingrese la marca: (nike, adidas, puma)(ver resultados en consola)"
-  );
-  entrada.toLowerCase();
-  busqueda();
-  menu2();
-}
-
-function busqueda() {
-  if (entrada == "nike" || entrada == "adidas" || entrada == "puma") {
-    const resultadosBusqueda = zapatillas.filter((el) =>
-      el.marca.includes(entrada)
-    );
-
-    console.log("Resultados:");
-    for (resultado of resultadosBusqueda) {
-      console.log(
-        `id: ${resultado.id}. Marca: ${resultado.marca}. Modelo: ${resultado.modelo}. Precio: $${resultado.precio}`
-      );
-    }
-  } else {
-    alert("No hay resultados en la búsqueda. Por favor intente nuevamente.");
-    buscarZapatillas();
-  }
-}
-
-// Finalizar Compra
-let total = 0;
-
-function calcularCarrito() {
-  for (articulo of carrito) {
-    total = total += articulo.precio;
-  }
-}
-
-function finalizarCompra() {
-  opcion = Number(
-    prompt(`Elegir método de pago:
-    1. Tarjeta de Débito. (20% off)
-    2. Tarjeta de Crédito. (Plan Ahora3, Ahora6 y Ahora12)`)
-  );
-
-  if (opcion == 1) {
-    total = (total * 0.8).toFixed(2);
-    alert(`El total de su orden es de $${total} en ${opcion} pago/s sin interés.
-                MUCHAS GRACIAS POR SU COMPRA`);
-  } else if (opcion == 2) {
-    let opcion = Number(
-      prompt(`Seleccione la cantidad de cuotas:
-        1. Pagar en 1 cuota sin interés.
-        3. Pagar en 3 cuotas sin interés.
-        6. Pagar en 6 cuotas sin interés.
-        12. Pagar en 12 cuotas sin interés.`)
-    );
-    if (opcion == 1) {
-      alert(`El total de su orden es de $${total} en ${opcion} pago sin interés.
-                MUCHAS GRACIAS POR SU COMPRA`);
-    } else if (opcion == 3) {
-      calcularCuotas(opcion);
-    } else if (opcion == 6) {
-      calcularCuotas(opcion);
-    } else if (opcion == 12) {
-      calcularCuotas(opcion);
-    } else {
-      alert("Ingrese un opción correcta.");
-      finalizarCompra();
-    }
-  } else {
-    alert("Ingrese un opción correcta.");
-    finalizarCompra();
-  }
-
-  function calcularCuotas(cantidad) {
-    let cuota = total / cantidad;
-    cuota = cuota.toFixed(2);
-    alert(`El total de su orden es de $${total} en ${cantidad} pagos sin interés de $${cuota}
-                MUCHAS GRACIAS POR SU COMPRA`);
-  }
-}
 
 checkStorage();
